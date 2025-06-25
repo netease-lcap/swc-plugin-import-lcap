c5841910dc2a6efd914eebbbd41a43c91c549a7f
御风 <18012261618@126.com>
2025-06-25 11:19:35 +0800

    test: unit tests

--- __test__/fixture/default/input.js
+++ __test__/fixture/default/input.js
@@ -1,2 +1,2 @@
-import { ElButton, ElInput } from "@lcap/element-plus";
+import { ElButton, ElInput, ConfigProvider } from "@lcap/element-plus";
 import { get, set } from "lodash";
--- __test__/fixture/default/output.js
+++ __test__/fixture/default/output.js
@@ -1,3 +1,4 @@
 import { ElButton } from "@lcap/element-plus/es/components/button/index";
 import ElInput from "@lcap/element-plus/es/components/input/index";
+import ConfigProvider from "@lcap/element-plus/es/ConfigProvider";
 import { get, set } from "lodash";
