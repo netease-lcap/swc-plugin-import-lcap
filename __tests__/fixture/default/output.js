e8a2cd284a86d47e14653264d8e1db69b76fa1b1
御风 <18012261618@126.com>
2025-07-22 11:45:05 +0800

    feat: 更新配置文件，添加组件源路径和默认值

--- __tests__/fixture/default/output.js
+++ __tests__/fixture/default/output.js
@@ -1,4 +1,5 @@
-import { ElButton } from "@lcap/element-plus/es/components/button/index";
-import ElInput from "@lcap/element-plus/es/components/input/index";
-import ConfigProvider from "@lcap/element-plus/es/ConfigProvider";
+import { ElButton } from "@lcap/element-plus/es/components/el-button/index";
+import { ElInput } from "@lcap/element-plus/es/components/el-input/index";
+import { ConfigProvider } from "@lcap/element-plus/es/components/el-config-provider/index";
+import { transformKeys } from "@lcap/element-plus/es/utils";
 import { get, set } from "lodash";
